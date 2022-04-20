10d1737634ff6a44c42b1ddc721382fb70bb979b
Alex Ramos <aletsoshi@outlook.com>
2022-04-19 23:28:44 -0500

    fix: :bug: create project fixed
    Ver compatibilidad del multer con prisma

--- src/components/projects/controller.js
+++ src/components/projects/controller.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 export const getProjects = async (req, res) => {
   try {
     const result = await prisma.project.findMany({
-      include: { equipo_trabajadores: true },
+      include: { responsable: true, equipo_trabajadores: true },
     });
     res.status(200).json(result);
   } catch (error) {
@@ -14,25 +14,45 @@ export const getProjects = async (req, res) => {
   }
 };
 
+// TODO: Revisar ya que antes daba un aray por defecto el select multiple de material UI, quiza nos ahorre chamba
 // TODO: Generar una condicional en caso no exista el id de la foreign key(para acciones, trabajadores, equipo_trabajadores y projects)
-
+// FIXME: No funciona la subida de archivos, probablemente incompatibilidad con multer
 //TODO: Pensar como debe ingresar los ids para trabajador https://www.google.com/search?q=add+many+to+many+prisma&oq=add+many+to+many+prisma&aqs=chrome..69i57.3001j0j1&sourceid=chrome&ie=UTF-8
 //*POST
 export const createProject = async (req, res) => {
+  if (
+    !req.body.archivo ||
+    req.body.archivo === "null" ||
+    req.body.archivo === "undefined"
+  ) {
+    req.body.archivo;
+  } else {
+    //console.log(req.file);
+    let serverUrl = req.protocol + "://" + req.get("host");
+    req.body.archivo = serverUrl + "/uploads/" + req.file.filename;
+  }
   try {
-    if (!req.body.archivo) {
-      req.body.archivo;
-    } else {
-      //console.log(req.file);
-      let serverUrl = req.protocol + "://" + req.get("host");
-      req.body.archivo = serverUrl + "/uploads/" + req.file.filename;
-    }
+    req.body.responsable_id = Number(req.body.responsable_id);
+    req.body.costo = Number(req.body.costo);
+    let identificacion = new Date(req.body.fecha_identificacion);
+    let inicio = new Date(req.body.fecha_inicio);
+    let cierre = new Date(req.body.fecha_cierre);
+    req.body.fecha_identificacion = identificacion.toISOString();
+    req.body.fecha_inicio = inicio.toISOString();
+    req.body.fecha_cierre = cierre.toISOString();
+    let equipo = req.body.equipo_trabajadores.split(",");
+    //const team2 = ["1", "2"];
     const result = await prisma.project.create({
       data: {
         ...req.body,
         equipo_trabajadores: {
-          create: [{ trabajador_id: 1 }, { trabajador_id: 2 }],
+          create: equipo.map((id) => ({
+            trabajador_id: Number(id),
+          })),
         },
+      },
+      include: {
+        equipo_trabajadores: true,
       },
     });
     res.status(201).json(result);
@@ -57,6 +77,7 @@ export const getProjectById = async (req, res) => {
   }
 };
 
+//? El set es perfecto para el update https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#set
 //*UPDATE
 export const updateProject = async (req, res) => {
   try {
