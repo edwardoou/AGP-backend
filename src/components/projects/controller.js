6ef5f6778ebf5aec7e6a8587e85f90586bd2c149
Alex Ramos <aletsoshi@outlook.com>
2022-05-02 19:51:54 -0500

    feat: :bug: Project functional

--- src/components/projects/controller.js
+++ src/components/projects/controller.js
@@ -12,13 +12,13 @@ export const getProjects = async (req, res) => {
         _count: true,
       },
     });
-    res.status(200).json(result);
+    res.status(200).json({ ok: true, data: result });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
-// TODO: Generar una condicional en caso no exista el id de la foreign key(para acciones, trabajadores, equipo_trabajadores y projects)
+// TODO: Generar una condicional en caso no exista el id para el UPDATE de la foreign key(para acciones, trabajadores, equipo_trabajadores y projects)
 // FIXME: No funciona la subida de archivos, probablemente incompatibilidad con multer
 //*POST
 export const createProject = async (req, res) => {
@@ -61,8 +61,9 @@ export const createProject = async (req, res) => {
       include: {
         responsable: { select: { nombre: true } },
         equipo_trabajadores: {
-          select: { trabajador_id: true },
+          select: { trabajadores: { select: { nombre: true } } },
         },
+        _count: true,
       },
     });
     res.status(201).json(result);
@@ -79,33 +80,75 @@ export const getProjectById = async (req, res) => {
       where: {
         id: Number(id),
       },
-      include: { equipo_trabajadores: true },
+      include: {
+        responsable: { select: { nombre: true } },
+        equipo_trabajadores: {
+          select: { trabajadores: { select: { nombre: true } } },
+        },
+        _count: true,
+      },
     });
-    res.status(200).json(result);
+    res.status(200).json({ ok: true, data: result });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
-//? El set es perfecto para el update https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#set
 //*UPDATE
 export const updateProject = async (req, res) => {
+  const { id } = req.params;
+  //Llega con comillas
+  if (
+    !req.body.archivo ||
+    req.body.archivo === "null" ||
+    req.body.archivo === "undefined"
+  ) {
+    req.body.archivo;
+  } else {
+    //console.log(req.file);
+    let serverUrl = req.protocol + "://" + req.get("host");
+    req.body.archivo = serverUrl + "/uploads/" + req.file.filename;
+  }
   try {
-    const { id } = req.params;
-    if (!req.body.archivo) {
-      req.body.archivo;
-    } else {
-      //console.log(req.file);
-      let serverUrl = req.protocol + "://" + req.get("host");
-      req.body.archivo = serverUrl + "/uploads/" + req.file.filename;
-    }
+    let workers = req.body.equipo_trabajadores.split(",");
+    //Elimina las antiguas relaciones
+    await prisma.equipoDeTrabajo.deleteMany({
+      where: {
+        project_id: Number(id),
+      },
+    });
     const result = await prisma.project.update({
       where: { id: Number(id) },
-      data: req.body,
+      data: {
+        ...req.body,
+        responsable_id: Number(req.body.responsable_id),
+        costo: Number(req.body.costo),
+        fecha_identificacion: new Date(
+          req.body.fecha_identificacion
+        ).toISOString(),
+        fecha_inicio: new Date(req.body.fecha_inicio).toISOString(),
+        fecha_cierre: new Date(req.body.fecha_cierre).toISOString(),
+        equipo_trabajadores: {
+          create: workers.map((id) => ({
+            trabajadores: {
+              connect: {
+                id: Number(id),
+              },
+            },
+          })),
+        },
+      },
+      include: {
+        responsable: { select: { nombre: true } },
+        equipo_trabajadores: {
+          select: { trabajadores: { select: { nombre: true } } },
+        },
+        _count: true,
+      },
     });
-    res.status(200).json(result);
+    res.status(200).json({ ok: true, data: result });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
@@ -113,13 +156,13 @@ export const updateProject = async (req, res) => {
 export const deleteProject = async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await prisma.project.delete({
+    await prisma.project.delete({
       where: {
         id: Number(id),
       },
     });
-    res.status(204).json(result);
+    res.status(204);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
