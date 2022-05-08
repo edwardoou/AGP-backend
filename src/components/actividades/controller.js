452725b246dcab3946356803a3392c01da384cbb
Alex Ramos <aletsoshi@outlook.com>
2022-05-08 18:19:24 -0500

    feat: actividades 1 ver

--- src/components/actividades/controller.js
+++ src/components/actividades/controller.js
@@ -3,20 +3,31 @@ import { prisma } from "../../db";
 //*GET
 export const getActividades = async (req, res) => {
   try {
-    const result = await prisma.actividad.findMany();
-    res.status(200).json(result);
+    const result = await prisma.actividad.findMany({
+      include: {
+        projects: { select: { nombre: true } },
+        trabajadores: { select: { nombre: true } },
+      },
+    });
+    res.status(200).json({ ok: true, data: result });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
 //*POST
 export const createActividad = async (req, res) => {
   try {
-    const result = await prisma.actividad.create({ data: req.body });
+    const result = await prisma.actividad.create({
+      data: req.body,
+      include: {
+        projects: { select: { nombre: true } },
+        trabajadores: { select: { nombre: true } },
+      },
+    });
     res.status(201).json(result);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
@@ -28,10 +39,14 @@ export const getActividadById = async (req, res) => {
       where: {
         id: Number(id),
       },
+      include: {
+        projects: { select: { nombre: true } },
+        trabajadores: { select: { nombre: true } },
+      },
     });
-    res.status(200).json(result);
+    res.status(200).json({ ok: true, data: result });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
@@ -39,14 +54,14 @@ export const getActividadById = async (req, res) => {
 export const deleteActividad = async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await prisma.actividad.delete({
+    await prisma.actividad.delete({
       where: {
         id: Number(id),
       },
     });
-    res.status(204).json(result);
+    return res.status(200).json({ ok: true });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
 
@@ -55,11 +70,17 @@ export const updateActividad = async (req, res) => {
   try {
     const { id } = req.params;
     const result = await prisma.actividad.update({
-      where: { id: Number(id) },
+      where: {
+        id: Number(id),
+        include: {
+          projects: { select: { nombre: true } },
+          trabajadores: { select: { nombre: true } },
+        },
+      },
       data: req.body,
     });
-    res.status(200).json(result);
+    res.status(200).json({ ok: true, data: result });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ ok: false, data: error.message });
   }
 };
